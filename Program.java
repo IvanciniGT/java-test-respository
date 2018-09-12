29aa5ac76143d771249a9cde5b4e4ac29e1f7a94
ivanciniGT <ivan.osuna.ayuste@gmail.com>
2018-09-12 07:29:39 +0200

    Main program

--- Program.java
+++ Program.java
@@ -1,6 +1,10 @@
-public class Programa{
+import org.junit.jupiter.api.Test;
 
-    public Programa(){}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Program{
+
+    public Program(){}
 
     public int suma(int op1, int op2){
         return op1+op2;
@@ -9,7 +13,13 @@ public class Programa{
     public static void main(String [] args){
         int op1=Integer.parseInt(args[0]);
         int op2=Integer.parseInt(args[1]);
-        System.out.println(new Programa.suma(op1,op2));
+        System.out.println(new Program().suma(op1,op2));
     }
 
+    @Test
+    public void sumar5y7(){
+        final int expected=12;
+        final int actual=new Program().suma(5,7);
+        assertEquals(actual,expected);
+    }
 }
